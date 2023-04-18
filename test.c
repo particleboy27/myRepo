99d42c8d97eb5096653bde1b515640397acb59cf
unknown <jessdoitnike@gmail.com>
2023-04-18 21:56:05 +0200

    commiting from windows vscode

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    int a;
+ 
+  printf("Enter an integer\n");
+  scanf("%d", &a);
+ 
+  printf("The integer is: %d\n", a);
+ 
+  return 0;
+
+}
