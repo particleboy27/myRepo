cc2ca3f7ae676bd2ed4a73d8579f967295730ef6
pewpewpoop <jessdoitnike@gmail.com>
2023-06-03 22:57:58 +0200

    q

--- adventOfCode2022/AOC2/aoc2.c
+++ adventOfCode2022/AOC2/aoc2.c
@@ -9,7 +9,9 @@ void main(){
 
     FILE * pfile;
     pfile = fopen("test.txt", "r");
-
+    if (pfile == NULL){ 
+    printf("File does not exist\n");
+    }
 
 
     fclose(pfile);
@@ -18,7 +20,7 @@ void main(){
     gettimeofday(&end, NULL);
     double time_taken = end.tv_sec + end.tv_usec / 1e6 -
                         start.tv_sec - start.tv_usec / 1e6; // in seconds
-                        
+
     printf("time program took %f seconds to execute\n", time_taken);
     
 }
