e43806ca5a579b003c8a94ad8280a280bb474dba
pewpewpoop <jessdoitnike@gmail.com>
2023-06-03 22:57:11 +0200

    preparing aoc2

--- adventOfCode2022/AOC2/aoc2.c
+++ adventOfCode2022/AOC2/aoc2.c
@@ -7,11 +7,18 @@ void main(){
     struct timeval start, end;
     gettimeofday(&start, NULL);
 
+    FILE * pfile;
+    pfile = fopen("test.txt", "r");
 
+
+
+    fclose(pfile);
+
+    
     gettimeofday(&end, NULL);
     double time_taken = end.tv_sec + end.tv_usec / 1e6 -
                         start.tv_sec - start.tv_usec / 1e6; // in seconds
-
+                        
     printf("time program took %f seconds to execute\n", time_taken);
     
 }
